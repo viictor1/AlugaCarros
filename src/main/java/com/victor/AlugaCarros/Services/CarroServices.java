7d6ec5081cca7e309b0cda3597c03d3ae783a91f
Victor <victorgaspalmeida@hotmail.com>
2023-05-20 12:20:41 -0300

    before

--- src/main/java/com/victor/AlugaCarros/Services/CarroServices.java
+++ src/main/java/com/victor/AlugaCarros/Services/CarroServices.java
@@ -1,25 +1,25 @@
 package com.victor.AlugaCarros.Services;
 
 import java.util.Date;
+import java.util.List;
 
 import com.victor.AlugaCarros.Enteties.Carro;
 import com.victor.AlugaCarros.Exceptions.CarroJaAlugadoException;
 import com.victor.AlugaCarros.Exceptions.CarroNullException;
 
 public class CarroServices {
-	public void alugar(Carro c) throws Exception {
-		
-		if (c.getAno() <= 0 || c.getMarca() == null || c.getModelo() == null || c.getAlugado() == null) {
-			throw new CarroNullException("Alguma atributo do carro é nulo");
-		}
-		
-		if (c.getAlugado() == true){
-			throw new CarroJaAlugadoException("Carro já está alugado");
-		}
-		
-		
-		c.setAlugado(true);
-		c.setDataInicio(new Date());
-
+	public void alugar(List<Carro> listaCarros) throws Exception {
+		for (Carro c : listaCarros) {
+			if (c.getAno() <= 0 || c.getMarca() == null || c.getModelo() == null) {
+				throw new CarroNullException("Alguma atributo do carro é nulo");
+			}
+			
+			if (c.getAlugado() == true){
+				throw new CarroJaAlugadoException("Carro já está alugado");
+			}
+			
+			c.setDataInicio(new Date());
+			c.setAlugado(true);
+		}	
 	}
 }
--- src/test/java/com/victor/AlugaCarros/Services/CarroServicesTest.java
+++ src/test/java/com/victor/AlugaCarros/Services/CarroServicesTest.java
@@ -2,47 +2,59 @@ package com.victor.AlugaCarros.Services;
 
 import static org.hamcrest.CoreMatchers.is;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.hamcrest.MatcherAssert;
 import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import com.victor.AlugaCarros.Enteties.Carro;
 
 public class CarroServicesTest {
 	
-	public Carro senario() throws Exception{
-		CarroServices service = new CarroServices();
-		Carro c = new Carro("uno", "fiat", 2004);
-
-		service.alugar(c);			
-		
-		return c;
+	private CarroServices service = new CarroServices();
+	List<Carro> listaCarros = new ArrayList<Carro>();
+	private Carro c1 = new Carro("uno", "fiat", 2004);
+	private Carro c2 = new Carro("gol", "bolinha", 2010);
+	
+	@BeforeEach
+	public void senario() throws Exception{	
+		listaCarros.add(c1);
+		listaCarros.add(c2);
+		service.alugar(listaCarros);
 	}
 	
 	@Test
 	public void testeAlugadoTrue() throws Exception {
-		Carro c = senario();
-		Assert.assertTrue(c.getAlugado());	
+		for (Carro c : listaCarros) {
+			Assert.assertTrue(c.getAlugado());				
+		}
 	}
 	
 	@Test
 	public void testeDataInicioIsHoje() throws Exception {
-		Carro c = senario();
-		Assert.assertTrue(c.getDataInicio().equals(new Date()));
+		for (Carro c : listaCarros) {
+			Assert.assertEquals(c.getDataInicio(), new Date());			
+		}
 	}
 	
 	@Test
 	public void testeDataFimIsNull() throws Exception {
-		Carro c = senario();
-		Assert.assertNull(c.getDataFim());
+		for (Carro c : listaCarros) {
+			Assert.assertNull(c.getDataFim());			
+		}
 	}
 	
 	@Test
 	public void testeAtributoCarroNull() {
 		CarroServices service = new CarroServices();
 		try {
-			service.alugar(new Carro());
+			List<Carro> list = new ArrayList<Carro>();
+			list.add(new Carro());
+			service.alugar(list);
 			Assert.fail();
 		} catch (Exception e) {
 			MatcherAssert.assertThat(e.getMessage(), is("Alguma atributo do carro é nulo"));
